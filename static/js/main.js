b9dadb1d6727aa06fa956a9e533430da9aa045ec
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2025-06-29 02:32:24 +0200

    feat: easter egg

--- static/js/main.js
+++ static/js/main.js
@@ -44,10 +44,26 @@ bannerImg.forEach(function(img) {
     img.addEventListener('click', function() {
         clickCount++;
 
+        // Remove any existing click classes
+        img.classList.remove('click-1', 'click-2', 'click-3', 'click-4', 'click-5');
+        
+        // Add the appropriate click class
+        img.classList.add(`click-${clickCount}`);
+        
+        // Remove the class after animation completes
+        setTimeout(() => {
+            img.classList.remove(`click-${clickCount}`);
+        }, clickCount === 5 ? 800 : 300 + (clickCount * 100));
+
         if (clickCount === 5) {
             let pageContent = document.querySelector('.content');
             pageContent.classList.toggle('upside-down');
-            clickCount = 0;
+            
+            // Reset click count and remove all click classes after a delay
+            setTimeout(() => {
+                clickCount = 0;
+                img.classList.remove('click-1', 'click-2', 'click-3', 'click-4', 'click-5');
+            }, 1000);
         }
     });
 });
