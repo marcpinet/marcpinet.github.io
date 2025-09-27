42a7bfd7c484a7d38674117c890f3e5c82064c40
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2025-09-27 21:56:24 +0200

    feat: add glassy hovering effect

--- static/js/main.js
+++ static/js/main.js
@@ -36,4 +36,25 @@ function switchTheme(e) {
 // event listener on checkbox change
 themeSwitcher.addEventListener('change', switchTheme, false);
 
+// Glassy highlight effect on cards
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.card');
+    
+    cards.forEach(card => {
+        card.addEventListener('mousemove', function(e) {
+            const rect = card.getBoundingClientRect();
+            const x = ((e.clientX - rect.left) / rect.width) * 100;
+            const y = ((e.clientY - rect.top) / rect.height) * 100;
+            
+            card.style.setProperty('--mouse-x', x + '%');
+            card.style.setProperty('--mouse-y', y + '%');
+        });
+        
+        card.addEventListener('mouseleave', function() {
+            card.style.setProperty('--mouse-x', '50%');
+            card.style.setProperty('--mouse-y', '50%');
+        });
+    });
+});
+
 
