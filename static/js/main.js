ef5c64b712623a0e653dc5cebe8533994448c671
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2025-10-02 00:09:50 +0200

    fix: light theme desktop

--- static/js/main.js
+++ static/js/main.js
@@ -1,40 +1,38 @@
-var themeSwitcher = document.querySelector('.theme-switcher input');
+var themeSwitchers = document.querySelectorAll('.theme-switcher input');
 var currentTheme = localStorage.getItem('theme');
 
-// check if there is a theme saved in local storage
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
     if (currentTheme === 'light') {
-        themeSwitcher.checked = true;
+        themeSwitchers.forEach(switcher => switcher.checked = true);
     }
 } else {
-    // If no theme is saved in local storage, use the user's system theme
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        // User prefers dark theme
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
     } else {
-        // User prefers light theme or the preference is not set, default to light theme
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
-        themeSwitcher.checked = true;
+        themeSwitchers.forEach(switcher => switcher.checked = true);
     }
 }
 
-// switch between themes
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
+        themeSwitchers.forEach(switcher => switcher.checked = true);
     } else {        
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+        themeSwitchers.forEach(switcher => switcher.checked = false);
     }    
 }
 
-// event listener on checkbox change
-themeSwitcher.addEventListener('change', switchTheme, false);
+themeSwitchers.forEach(switcher => {
+    switcher.addEventListener('change', switchTheme, false);
+});
 
 // Glassy highlight effect on cards
 document.addEventListener('DOMContentLoaded', function() {
