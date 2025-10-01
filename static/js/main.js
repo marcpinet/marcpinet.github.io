18de2c88befc930d909c92d2a1dd43b4da408177
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2025-10-01 14:15:10 +0200

    feat: cards are now globally clickable

--- static/js/main.js
+++ static/js/main.js
@@ -49,11 +49,6 @@ document.addEventListener('DOMContentLoaded', function() {
             card.style.setProperty('--mouse-x', x + '%');
             card.style.setProperty('--mouse-y', y + '%');
         });
-        
-        card.addEventListener('mouseleave', function() {
-            card.style.setProperty('--mouse-x', '50%');
-            card.style.setProperty('--mouse-y', '50%');
-        });
     });
     
     // Make field tags clickable with Wikipedia links
